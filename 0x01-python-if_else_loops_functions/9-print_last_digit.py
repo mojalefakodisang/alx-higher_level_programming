0e7f22d17b4369b3aec2563ea1ad1f5f50338c48
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-05 13:56:27 -0700

    Prints last digit of an integer

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def print_last_digit(number):
- 
-    if number > 9:
-        n = str(number)
-        n = n[-1]
-    else:
+
+    if number >= 0 and number <= 9:
         n = number
+    else:
+        n = abs(number)
+        n = str(n)
+        n = n[-1]
+
     print('{}'.format(n), end="")
 
     return (n)
