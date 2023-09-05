bf2d6360f7ef193da0cc041d777fca84b37dae8e
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-05 13:27:53 -0700

    Converts strings to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for ch in str:
-        ch = chr(ord(ch) - 32)
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            ch = chr(ord(ch) - 32)
         print("{:s}".format(ch), end="")
 
     print("\n", end="")
