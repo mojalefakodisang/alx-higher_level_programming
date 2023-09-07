5214b1a00f68538de1a239e54061163c3e837ec1
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-07 11:25:37 -0700

    [Task 6] - Calculator from arguments

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    args = len(sys.argv) - 1
+    if args != 3:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        op = sys.argv[2]
+
+        if op == "+":
+            print("{0} + {1} = {2}".format(a, b, add(a, b)))
+        elif op == "-":
+            print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+        elif op == "*":
+            print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+        elif op == "/":
+            print("{0} / {1} = {2}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Availabe operators: +, -, * and /")
+            exit(1)
