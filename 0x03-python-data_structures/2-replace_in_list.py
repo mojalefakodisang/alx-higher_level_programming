4574ea03ac39d2c022b5038394dfd412be488373
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-10 13:10:38 -0700

    [Task 2] - Replaces element at index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,8 +8,9 @@ def replace_in_list(my_list, idx, element):
         Return: returns a new list with replaced elements
     """
     if idx < 0:
-        return None
+        return my_list
     if idx > len(my_list) - 1:
-        return None
+        return my_list
+
     my_list[idx] = element
     return my_list
