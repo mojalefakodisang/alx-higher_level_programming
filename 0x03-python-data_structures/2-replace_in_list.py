7cff39eee8a5d918b3a36e9f911d62101de17d88
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-10 13:06:48 -0700

    [Task 2] - Replaces element at index

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """Replaces element at a certain index within a list
+        my_list: input list
+        idx: index at which you are to replace with element
+        element: element to be added
+
+        Return: returns a new list with replaced elements
+    """
+    if idx < 0:
+        return None
+    if idx > len(my_list) - 1:
+        return None
+    my_list[idx] = element
+    return my_list
