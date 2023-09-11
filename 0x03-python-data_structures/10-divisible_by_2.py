a130b4863640974866dc30a3b2a78781d914a7b0
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-11 12:01:12 -0700

    [Task 10] - Returns a list of bool to check if integers in a list are divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Checks if the contents of a list are divisible by 2
+        my_list: input list
+
+        Return: returns a list of bool that checks integers are divisible by 2
+    """
+    list_result=[]
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            list_result.append(True)
+        else:
+            list_result.append(False)
+    return list_result
