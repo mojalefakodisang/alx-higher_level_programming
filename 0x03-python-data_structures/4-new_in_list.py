a0df955db35b2e9a6d94cf6236e8b1d58bf7ab91
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-11 05:02:56 -0700

    [Task 4] - Replaces elemnet at a specific index with new element

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,8 +7,9 @@ def new_in_list(my_list, idx, element):
 
         Return: returns original list else a replaced list
     """
+    copy = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        copy[idx] = element
+        return copy
