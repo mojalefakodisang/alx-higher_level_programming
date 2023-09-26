3d3358340e9dd16fbc2480f403152d889487ed6d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-25 19:42:53 -0700

    [Update] - Safely divides 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,7 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             new_list.append(0)
             print("division by zero")
-        except TypeError: 
+        except TypeError:
             new_list.append(0)
             print("wrong type")
         except IndexError:
