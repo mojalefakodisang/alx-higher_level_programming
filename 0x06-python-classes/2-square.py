26809074774c5d6563e289ea4f3366aa0c31b4a8
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-27 07:27:53 -0700

    [Update] - Added class with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represents a squre"""
+
+    def __init__(self, size):
+        """ Function that initialize class Square
+
+            Args:
+                size (int): size attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size nust be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
