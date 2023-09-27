5acdb4b3fdd7f4ad87dbbc6c470830bf92eda6e6
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-27 11:12:11 -0700

    [Update] - Prints square of # and space before

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,11 +47,10 @@ class Square:
     @position.setter
     def position(self, value):
         """Method that sets position"""
-        if not isinstance(value, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2 or not all(isinstance(num, int) for num in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(num >= 0 for num in value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
