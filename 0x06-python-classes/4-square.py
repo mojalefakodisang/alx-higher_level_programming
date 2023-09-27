c08e522f238f411f533d28289db4bae68ace966d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-27 08:41:16 -0700

    [Update] - Added setter and getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -34,6 +34,6 @@ class Square:
         """Method that sets size of square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
