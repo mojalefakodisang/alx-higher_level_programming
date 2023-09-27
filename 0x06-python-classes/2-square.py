523997fca81990b369624279e5f834bfa1c6d2f2
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-27 07:50:58 -0700

    [Update] - Added class with exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,6 +13,6 @@ class Square:
         """
         self.__size = size
         if not isinstance(size, int):
-            raise TypeError("size nust be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
