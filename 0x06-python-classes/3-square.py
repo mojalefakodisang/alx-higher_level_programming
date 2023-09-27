45b1ca09b88173509e9b8e323a4763e4aa2d4d66
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-09-27 08:12:02 -0700

    [Update] - Has a class that returns square of size

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module with a clss Square """
+
+
+class Square:
+    """ Clase that takes in a size and returns its area"""
+
+    def __init__(self, size):
+        """Method that initializes Square class
+
+            Args:
+                size: size attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Method that return square of a size
+
+            Return: returns the square of size
+        """
+        return self.__size ** 2
