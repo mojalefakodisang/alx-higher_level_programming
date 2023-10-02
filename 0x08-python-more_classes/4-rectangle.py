b836bbb3b6b637741a6c42fa2129c603201af0ee
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-02 06:39:47 -0700

    [Task 4] - Representation of the rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -72,4 +72,4 @@ class Rectangle:
 
     def __repr__(self):
         """ Representation of rectangle"""
-        return "Rectangle ({:d}, {:d})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
