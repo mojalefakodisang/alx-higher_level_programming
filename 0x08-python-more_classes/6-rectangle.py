fc28f96d341fa8213c22b85df2d3ca83b14800ac
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-02 06:50:16 -0700

    [Task 6] - Gets number of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """ Class Rectangle"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Method that initializes attributes"""
         self.width = width
