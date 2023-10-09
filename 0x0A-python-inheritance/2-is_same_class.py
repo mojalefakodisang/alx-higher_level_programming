6b47c2499015b837f4330de57b3599363084eb9d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-09 09:03:22 -0700

    [Task 2] - Checks if an object is part of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,8 +4,6 @@
 
 def is_same_class(obj, a_class):
     """Checks if obj is part of a_class"""
-    if obj == None or a_class == None:
-        return False
-    if (not isinstance(obj, a_class)):
-        return False
-    return True
+    if type(obj) == a_class:
+        return True
+    return False
