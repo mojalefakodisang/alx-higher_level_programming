ebab1ee75a1c97d31f6ad6b7ede6f38a0a1881f6
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-09 13:37:55 -0700

    [Task 10] - Calculate area of a square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,5 +12,6 @@ class Square(Rectangle):
             Args:
                 size: size of the square
         """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
