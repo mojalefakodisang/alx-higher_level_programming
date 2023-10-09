54598b1d0addb55c52c90f1483e6ae7c0435ff4a
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-09 13:46:46 -0700

    [Task 11] - Calculate area of a square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,3 +15,11 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """Calculates area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """super method"""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
