eab900d34360719216cef938749a4a6ca123fc1b
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-09 13:36:39 -0700

    [Task 10] - Calculate area of a square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""The '10-square' module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class square which inherited attributes of Rectangle"""
+
+    def __init__(self, size):
+        """Initiates class Square
+
+            Args:
+                size: size of the square
+        """
+        super().__init__(size, size)
+        self.__size = size
