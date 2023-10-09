0b7bef01258650d977d038bfe3c7004e2bb98d26
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-09 10:24:07 -0700

    [Task 6] - A class with method area that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""The '5-base_geometry.py' module"""
+
+
+class BaseGeometry:
+    """The BaseGeometry class"""
+
+    def area(self):
+        """Area of BaseGeometry class"""
+        raise Exception("area() is not implemented")
