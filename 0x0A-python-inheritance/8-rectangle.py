bda7d3a98a9c2d93b73569deec1701f76ddc4efc
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-09 13:19:11 -0700

    [Task 8] - Validates width and height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The '8-rectangle module"""
-BaseGeometry =  __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
