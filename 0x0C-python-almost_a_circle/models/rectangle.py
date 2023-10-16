cb7451d64b162bc57fe13b87a6e4c7c88f77da62
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-16 11:04:18 -0700

    [Update] - Fixed Rectangle.py for pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Gets width of the rectangle"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -43,7 +43,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Gets height of the rectangle"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -57,7 +57,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Gets x of the rectangle"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -71,7 +71,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Gets y of the rectangle"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -100,7 +100,7 @@ class Rectangle(Base):
             a = 0
             for arg in args:
                 if a == 0:
-                    if arg == None:
+                    if arg is None:
                         self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = arg
@@ -116,7 +116,7 @@ class Rectangle(Base):
         elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "id":
-                    if v == None:
+                    if v is None:
                         self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = v
