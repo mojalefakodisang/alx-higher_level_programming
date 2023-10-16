7891c800293980ee30a71dbfeb5d6e18e20c09bd
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-16 11:33:50 -0700

    [Update] - Updated few methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,8 +42,8 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""
-        if json_string is None:
-            return "[]"
+        if json_string is None or json_string == "[]":
+            return []
         else:
             return json.loads(json_string)
 
