6f79d3eeebcab8cc9b5049c2a78432729434b8ee
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-11-14 12:20:10 -0800

    [Update] - All other scripts

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Script that creates the database hbtn_0c_0
+-- If the database hbtn_0c_0 already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- Script taht show all records of first_table
+SELECT *
+FROM first_table;
