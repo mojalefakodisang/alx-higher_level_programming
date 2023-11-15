6d88813546c04591c763a836105bf348e46e2a8b
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-11-15 09:36:27 -0800

    [Update] - Lists all privilages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Sc=ript that lists all privileges on MySQL users
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
