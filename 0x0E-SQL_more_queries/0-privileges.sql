3320271dd2e2ea211d40acabe164cafa52dc7c5d
Mojalefa Kodisang <105329271+mojalefakodisang@users.noreply.github.com>
2023-12-21 19:48:45 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/0-privileges.sql
+++
@@ -1,4 +0,0 @@
--- Sc=ript that lists all privileges on MySQL users
-
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++
@@ -1,5 +0,0 @@
--- Lists all cities in database
-
-SELECT cities.id, cities.name, states.name FROM cities
-LEFT JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
