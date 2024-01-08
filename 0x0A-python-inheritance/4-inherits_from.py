2517e3878760d4aa5fddaa0efd177a3fb5de7b52
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-08 01:26:34 -0800

    [Task 4] - Checks if an object is inherited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Function defined"""
-    return issubclass(obj.__class__, a_class) and obj.__class != a_class
+    return issubclass(obj.__class__, a_class) and obj.__class__ != a_class
