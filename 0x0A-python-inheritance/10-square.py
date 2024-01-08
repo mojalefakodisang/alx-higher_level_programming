e2ea0090dc7b4a8c87ef013e02ed1f5a50cb0832
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-08 08:46:43 -0800

    [Task 10] - fixed initiation

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Method that initiates Square class"""
-        self.integer_validator("size", size)
+        super().integer_validator("size", size)
         super.__init__(size, size)
         self.__size = size
 
