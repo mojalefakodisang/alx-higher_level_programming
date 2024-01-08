85a752e9bdc3c49d6c8177015468b28a9bf090de
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-08 00:18:46 -0800

    [Task 6] - FIxed the entire file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+"""Module containing unittest of Max integer"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    i = 1
-    result = list[0]
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
+class TestMaxInteger(unittest.TestCase):
+    """class that tests for max integer in a list"""
 
-    return result
+    def test_empty_list(self):
+        """Tests when the list is empty"""
+        self.assertIsNone(max_integer([]))
+
+    def test_normal_list(self):
+        """Tests if a normal list is passed"""
+        self.assertEqual(max_integer([10, 1, 2, 3]), 10)
+
+    def test_normal_list_beginning(self):
+        """Tests if a normal list is passed and max integer at the beginning"""
+        self.assertEqual(max_integer([10, 1, 2, 3, 4]), 10)
+
+    def test_normal_list_end(self):
+        """Tests if a normal list is passed and max integer at the end"""
+        self.assertEqual(max_integer([1, 2, 3, 4, 10]), 10)
+
+    def test_one_element_list(self):
+        """Tests if a list has one element"""
+        self.assertEqual(max_integer([10]), 10)
+
+    def test_if_list(self):
+        self.assertIsInstance(max_integer([10, 1, 2, 3, 4]), int)
