ee737f3934295547804c1effcdd4edc38c202648
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-08 01:15:53 -0800

    [Task 2] - Checks if object is of a certain class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that contains is_same_class method"""
+
+
+def is_same_class(obj, a_class):
+    """Method that checks if obj is of same class"""
+    return type(obj) is a_class
