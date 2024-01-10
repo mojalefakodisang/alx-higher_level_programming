116330a542652892b2ae0b1a852a61842f9a5201
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-10 02:56:56 -0800

    [Task 8] - fixed

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module containing class_to_json method"""
+
+
+def class_to_json(obj):
+    """returns the dictionary representation"""
+    all_vars = vars(obj)
+    return all_vars
