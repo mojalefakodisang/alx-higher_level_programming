09a93ca13e3db5e99e68045c6fc5a4a8c4f915f3
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-10 03:10:34 -0800

    [Task 10] - Students

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module containing a Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrives a dictionary representation"""
+        flag = 0
+        all = vars(self)
+        if type(attrs) is not list:
+            flag = 1
+        else:
+            for el in attrs:
+                if type(el) is not str:
+                    flag = 1
+
+        if flag == 1:
+            return all
+        else:
+            return dict((key, all[key]) for key in attrs if key in all)
