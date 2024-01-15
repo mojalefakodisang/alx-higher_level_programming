ed1c0e92235c4e59ebfd4abc478d7d2258ce69cb
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-15 09:06:58 -0800

    [Update] - Fixed the to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,8 @@ class Base():
         """Returns the JSON string representation of
         list_dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == []:
+        if (list_dictionaries is None or list_dictionaries == [] or 
+        type(list_dictionaries) != list):
             return "[]"
 
         return json.dumps(list_dictionaries)
