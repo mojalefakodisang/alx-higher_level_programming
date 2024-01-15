a261905abc29f1975d97fcd9a8f62481412b8468
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-15 09:13:55 -0800

    [Update] - Reversed changes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,8 +28,7 @@ class Base():
         """Returns the JSON string representation of
         list_dictionaries
         """
-        if (list_dictionaries is None or list_dictionaries == [] or 
-        type(list_dictionaries) != list):
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
 
         return json.dumps(list_dictionaries)
