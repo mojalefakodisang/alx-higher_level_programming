2b11f6b4f13b972cbfb0b5374a19be805d925e78
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-02-14 11:32:57 -0800

    ** Fixed errors **

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_gernes.genre_id IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Number of shows by genre
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_gernes
+FROM tv_genres
 JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
