5f4ce13b9037bd203d1e0f828be9190ece16801b
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-03-11 17:50:36 +0200

    [Task 6] - Prints multiple languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+/* Prints multiple languages */
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < languages.length; i++) console.log(languages[i]);
