5616c67d07d2109d57fef384c82debec9b5e2035
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-03-11 22:08:07 +0200

    [Update] - Fixed Logic

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 /* Prints a square */
 const { argv } = require('node:process');
 const size = parseInt(argv[2]);
-if (size === 'NaN' || argv[2] === undefined) {
+if (isNaN(size) || argv[2] === undefined) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
