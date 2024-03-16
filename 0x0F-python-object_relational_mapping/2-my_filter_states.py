211c9e8db25f9fe902865a485c84817d0a290878
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-16 18:54:43 +0200

    [Update] - Filter by state_name in the arguments

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Displays all values with state_name in argument"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    USER = sys.argv[1]
+    PWD = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(user=USER, passwd=PWD, db=DB_NAME)
+    cur = db.cursor()
+    cur.execute("""SELECT id, name
+                FROM states
+                WHERE name = %s""", (state_name, ))
+    all = cur.fetchall()
+    for i in all:
+        print(i)
