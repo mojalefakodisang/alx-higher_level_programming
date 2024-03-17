c4c6b18373cf23ad8424c6c95eb9b6abed3216ad
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-17 10:37:01 +0200

    [Update] - Adds a new state Louisiana

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Adds a new state into the database"""
+import sys
+from model_state import State, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.\
+        format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    state = State(name="Louisiana")
+    for i in state:
+        print(i.id)
+    session.add(state)
+    session.commit()
+    session.close()
