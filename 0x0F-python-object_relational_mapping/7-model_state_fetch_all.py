5c7d95d7f3d73b9100564ef364bb6dc7f4d3f333
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-17 09:36:01 +0200

    [Update] - Make a query about states in hbtn_0e_6_usa database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Fetch all states"""
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    ), pool_pre_ping=True)
+
+    '''Make a session'''
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    states = session.query(State.name)
+
+    '''Length of the query'''
+    states_len = session.query(State.name).count()
+
+    for i in range(states_len):
+        print("{}: {}".format(i + 1, states[i][0]))
