430fa4db868bbc9153fa99d7086999d9c10ea495
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-28 17:11:51 +0200

    [Update] - Request header of a URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,17 +2,10 @@
 import urllib.request
 
 url = 'http://alx-intranet.hbtn.io/status'
-
-try:
-    with urllib.request.urlopen(url) as response:
-        content_type = response.headers.get('Content-Type')
-        content = response.read()
-        utf_content = content.decode('utf8')
-        heading = f"Body response:\n"
-        c_type = f"\t- type: {type(content)}\n"
-        c_content = f"\t- content {content}\n"
-        c_utf = f"\t- utf8 content: {utf_content}"
-
-        print(heading + c_type + c_content + c_utf)
-except Exception as e:
-    print(e)
+req = urllib.request.Request(url)
+print("Body response:")
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf8')}")
