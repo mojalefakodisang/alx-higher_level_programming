98a40dd7f22a843bac5afa4f86389eb746178499
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-28 20:04:40 +0200

    [Update] - Auth GitHub API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,12 +7,11 @@ takes your GitHub credentials
 if __name__ == '__main__':
     import requests
     import sys
-    url = 'https://api.githhub.com/user'
+    url = 'https://api.github.com/user'
     username = sys.argv[1]
     password = sys.argv[2]
     response = requests.get(url, auth=(username, password))
-    json = response.json()
-    if 'id' in json:
-        print(json['id'])
+    if 'id' in response.json():
+        print(response.json()['id'])
     else:
         print(None)
