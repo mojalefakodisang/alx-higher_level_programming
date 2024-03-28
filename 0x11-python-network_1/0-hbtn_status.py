3a11a7263e45bf56197f8ac0903c46c0aac59bfe
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-28 17:13:34 +0200

    [Update] - Request header of a URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
+""" Script that fetches a URL"""
 
-url = 'http://alx-intranet.hbtn.io/status'
-req = urllib.request.Request(url)
-print("Body response:")
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content.decode('utf8')}")
+
+if __name__ == "__main__":
+    import urllib.request
+    url = 'http://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+    print("Body response:")
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf8')}")
