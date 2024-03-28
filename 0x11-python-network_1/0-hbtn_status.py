c36eadb1f23b2a828b414a315fc7b57c2b425c22
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-28 16:58:52 +0200

    [Update] - Request header of a URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = 'http://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    content_type = response.headers.get('Content-Type')
+    content = response.read()
+    utf_content = content.decode('utf8')
+    heading = f"Body response:\n"
+    c_type = f"\t- type: {type(content)}\n"
+    c_content = f"\t- content {content}\n"
+    c_utf = f"\t- utf8 content: {utf_content}"
+
+print(heading + c_type + c_content + c_utf)
