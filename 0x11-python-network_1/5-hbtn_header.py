07c678131d53b72bda214dabde6c09f4beccc2b6
Mojalefa Kodisang <ykodisangmojalefa65@gmail.com>
2024-03-28 18:35:23 +0200

    [Update] - sends a request to the URL and displays the value of the variable X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+sends a request to the URL and displays the value of the variable X-Request-Id
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers['X-Request-Id'])
