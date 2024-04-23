a9e07305f4d2683b7dfb071bf540b901404b620b
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-04-23 14:45:48 -0700

    [Update] - Fixed indetations

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-const requestURL = process.argv[2];
-request(requestURL, (error, response, body) => {
-	if (!error) {
-		console.log(`code: ${response.statusCode}`);
-	}
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (!error) {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
