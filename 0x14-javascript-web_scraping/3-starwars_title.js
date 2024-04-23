2bbeac553f622c6b4cf4b8fa74b375625297f8ef
Mojalefa Kodisang <kodisangmojalefa65@gmail.com>
2024-04-23 15:11:08 -0700

    [Update] - StarWars Titles

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,8 @@ const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const movieUrl = url + movieId;
 request(movieUrl, (error, response, body) => {
-	if (!error) {
-		content = JSON.parse(body);
-		console.log(content.title);
-	}
+  if (!error) {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
 });
